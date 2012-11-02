b68c762c6e47f6b8f4356ade825952aff6595e0a
AOSP-ZEUS <dman3285@gmail.com>
2012-11-02 09:15:38 -0400

    add LiquidSmooth and packages

--- Repo_Sync.sh
+++ Repo_Sync.sh
@@ -10,17 +10,20 @@ do
 
   clear
   echo 
-  echo "================================================="
-  echo "*               AOSP-SYNC V-1                   *"
-  echo "================================================="  
-  echo "                1 - Sync CM10                   *"
-  echo "                2 - Sync AOKP                   *"
-  echo "                3 - Sync AoCP                   *"
-  echo "                4 - Sync CM9                    *"
-  echo "                5 - Install Java-6              *"
-  echo "                6 - Install Required Packages   *"
-  echo "------------------------------------------------*"
-  echo "                  x - Exit"
+  echo "#################################################"
+  echo "#               AOSP-SYNC V-1                   #"
+  echo "#===============================================#"  
+  echo "#               1 - Sync CM10                   #"
+  echo "#               2 - Sync AOKP Jellybean         #"
+  echo "#               3 - Sync AoCP Jellybean         #"
+  echo "#               4 - Sync CM9                    #"
+  echo "#               5 - Sync AOSP Jellybean         #"
+  echo "#               6 - Sync LiquidSmooth JB        #"
+  echo "#               7 - Install Java-6              #"
+  echo "#               8 - Install Required Packages   #"
+  echo "#################################################"
+  echo "#                 x - Exit                      #"
+  echo "#################################################"
   echo 
   echo -n "Enter option: "
   read opt
@@ -32,8 +35,10 @@ do
       2) sh tools/AOKP.sh; echo "Done.";;     
       3) sh tools/AoCP.sh; echo "Done.";;
       4) sh tools/CM9.sh; echo "Done.";;
-      5) sh tools/java-6.sh; echo "Done";;
-      6) sh tools/packages.sh; echo "Done";;
+      5) sh tools/AOSP-JB.sh; echo "Done";;
+      6) sh tools/Liquid.sh; echo "Done";;
+      7) sh tools/java-6.sh; echo "Done";;
+      8) sh tools/packages.sh; echo "Done";;
       x) clear; echo; echo "Goodbye."; echo; exit 1;;
       *) echo "Invalid option"; continue;;
     esac
