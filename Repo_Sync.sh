f02715477948bbc1e1b6359683014edaedd010b5
AOSP-ZEUS <dman3285@gmail.com>
2012-11-04 11:44:20 -0500

    Add CNA and organize options

--- Repo_Sync.sh
+++ Repo_Sync.sh
@@ -10,20 +10,21 @@ do
 
   clear
   echo 
-  echo "#################################################"
-  echo "#               AOSP-SYNC V-1                   #"
-  echo "#===============================================#"  
-  echo "#               1 - Sync CM10                   #"
-  echo "#               2 - Sync AOKP Jellybean         #"
-  echo "#               3 - Sync AoCP Jellybean         #"
-  echo "#               4 - Sync CM9                    #"
-  echo "#               5 - Sync AOSP Jellybean         #"
-  echo "#               6 - Sync LiquidSmooth JB        #"
-  echo "#               7 - Install Java-6              #"
-  echo "#               8 - Install Required Packages   #"
-  echo "#################################################"
-  echo "#                 x - Exit                      #"
-  echo "#################################################"
+  echo "##########################################################"
+  echo "#               AOSP-SYNC V-1                            #"
+  echo "#========================================================#"  
+  echo "#               1 - Install Java-6                       #"
+  echo "#               2 - Install Required Packages            #"
+  echo "#               3 - Sync AoCP Jellybean                  #"
+  echo "#               4 - Sync CM9                             #"
+  echo "#               5 - Sync AOSP Jellybean                  #"
+  echo "#               6 - Sync LiquidSmooth JB                 #"
+  echo "#               7 - sync CNA Jellybean                   #"
+  echo "#               8 - Sync CM10                            #"
+  echo "#               9 - Sync AOKP                            #"
+  echo "##########################################################"
+  echo "#                      x - Exit                          #"
+  echo "##########################################################"
   echo 
   echo -n "Enter option: "
   read opt
@@ -31,14 +32,15 @@ do
   if [ "$?" != "1" ]
   then
     case $opt in
-     1) sh tools/CM10.sh; echo "Done.";;
-      2) sh tools/AOKP.sh; echo "Done.";;     
+     1) sh tools/java-6.sh; echo "Done.";;
+      2) sh tools/packages.sh; echo "Done.";;     
       3) sh tools/AoCP.sh; echo "Done.";;
       4) sh tools/CM9.sh; echo "Done.";;
       5) sh tools/AOSP-JB.sh; echo "Done";;
       6) sh tools/Liquid.sh; echo "Done";;
-      7) sh tools/java-6.sh; echo "Done";;
-      8) sh tools/packages.sh; echo "Done";;
+      7) sh tools/CNA.sh; echo "Done";;
+      8) sh tools/CM10.sh; echo "Done";;
+      9) sh tools/AOKP.sh; echo "Done";;
       x) clear; echo; echo "Goodbye."; echo; exit 1;;
       *) echo "Invalid option"; continue;;
     esac
